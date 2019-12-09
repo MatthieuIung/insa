fd300f48f98dc62d9e4d030bd2ee439223dc2fe2
Matthieu IUNG <etudiant2@machine2.cri15.ensi-bourges.fr>
2019-12-09 16:38:00 +0100

    Le commit du Jour 1

--- poneys/src/Poneys.php
+++ poneys/src/Poneys.php
@@ -11,7 +11,7 @@ class Poneys
      *
      * @return void
      */
-    public function getCount(): int
+    public function getCount()
     {
         return $this->count;
     }
@@ -23,9 +23,32 @@ class Poneys
      *
      * @return void
      */
-    public function removePoneyFromField(int $number): void
+    public function removePoneyFromField(int $number)
     {
-        $this->count -= $number;
+        if($number > $this->count){
+            echo "\n ", $this->count, ' sont retirés du champ et tu n as pas réussi a en retirer ', $number-$this->count;
+            $this->count=0;
+            throw new Exception('Tu ne peux pas retirer des Poneys qui n existent pas');
+        }
+        else if($number < 0){
+            throw new Exception('nombre négatifs');
+        }
+        $this->count -= $number;        
+    }
+
+    /**
+     * Ajoute un poney du champ
+     *
+     * @param int $number Nombre de poneys à ajouter
+     *
+     * @return void
+     */
+    public function addPoneyFromField(int $number)
+    {
+        if(!$this->isFree()){
+            throw new Exception('Plus de place pour des poneys');
+        }
+        $this->count += $number;
     }
 
     /**
@@ -33,9 +56,23 @@ class Poneys
      *
      * @return array
      */
-    public function getNames(): array
+    public function getNames()
     {
 
     }
+
+    /**
+     * Retourne si il reste des places dans le champs. true si dispo sinon false
+     *
+     * @return boolean
+     */
+    public function isFree()
+    {
+        if($this->count < 15){
+            return true;
+        }
+        return false; 
+    }
+
 }
 ?>
--- poneys/tests/PoneysTest.php
+++ poneys/tests/PoneysTest.php
@@ -21,8 +21,101 @@ class PoneysTest extends TestCase
         // Action
         $Poneys->removePoneyFromField(3);
 
+
         // Assert
         $this->assertEquals(5, $Poneys->getCount());
     }
+
+    public function testaddPoneyFromField()
+    {
+        // Setup
+        $Poneys = new Poneys();
+
+        // Action
+        $Poneys->addPoneyFromField(1);
+
+
+        // Assert
+        $this->assertEquals(9, $Poneys->getCount());
+    }
+
+
+    //on en retire plus qu'il y en a 
+    public function testRemovePoneyFromField2()
+    {
+        // Setup
+        $Poneys = new Poneys();
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("\n\nTrop de poneys retirés");
+
+        // Action
+        $Poneys->removePoneyFromField(10);
+    }
+
+
+    /**
+     * @dataProvider removePoneysProvider
+     *
+     * @return void
+     */
+    public function testRemovePoneyFromField3($substract,$count)
+    {
+        // Setup
+        $Poneys = new Poneys();
+
+        // Action
+        $Poneys->removePoneyFromField($substract);
+
+        // Assert
+        $this->assertEquals($count, $Poneys->getCount());
+    }
+
+    public function removePoneysProvider()
+    {
+        return array(
+            array(5, 3),
+            array(4, 4),
+            array(2, 6),
+            array(0, 8),
+            array(1, 7),
+            array(2, 7)
+        );
+    }
+
+    public function test_getNames(){
+        $names = ['Joe', 'William', 'Jack', 'Averell'];
+        $this->poneys = $this->getMockBuilder('Poneys')->getMock();
+        $this->poneys->expects($this->exactly(1))->method('getNames')->willReturn($names);
+        $this->assertEquals($names, $this->poneys->getNames());
+    }
+
+    /**
+     * @dataProvider addPoneysProvider
+     *
+     * @return void
+     */
+    public function testAddPoneyFromField2($substract,$count)
+    {
+        // Setup
+        $Poneys = new Poneys();
+
+        // Action
+        $Poneys->addPoneyFromField($substract);
+
+        // Assert
+        $this->assertEquals($count, $Poneys->getCount());
+    }
+
+    public function addPoneysProvider()
+    {
+        return array(
+            array(2, 10),
+            array(3, 11),
+            array(4, 12),
+            array(8, 16),
+            array(7, 10)
+        );
+    }
+
 }
 ?>
